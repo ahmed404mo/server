21c832e188d9d11f9ad021e4031f8282b08e2dea
ahmed mokhtar <mo879938@gmail.com>
2025-09-16 14:27:30 +0300

    solve -04

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <h1>مرحبًا 👋</h1>
-      <p>الموقع شغال بنجاح 🎉</p>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#f0f4f8",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      <h1 style={{ color: "#f0f4f8", fontSize: "3rem", backgroundColor: ' #2c3e50',margin: 0,padding: '10px 27%' }}>Welcom admin</h1>
+      <p style={{ color: "#34495e", fontSize: "1.5rem" ,backgroundColor: '#b9b9b9ff',margin: 0,padding: '10px 27%' }}>
+        Acess For Server
+      </p>
     </div>
   )
 }
